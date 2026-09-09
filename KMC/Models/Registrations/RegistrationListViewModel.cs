39469ba54bdcaa5b7514355dbd4b458476ef9efe
Isuru Akash <isuru.a.mallawa@gmail.com>
2026-09-09 07:30:41 +0530

    Delete KMC/Models directory

--- KMC/Models/Registrations/RegistrationListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KMC.Web.Models.Registrations;
-
-public class RegistrationListViewModel
-{
-    public IReadOnlyList<RegistrationViewModel> Registrations
-    { get; set; } = [];
-
-    public string? ErrorMessage { get; set; }
-}
